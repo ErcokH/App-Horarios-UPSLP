a421a73bec41f8e619906e85f47880f3f8b8c119
ErcokH <Enrique.Munguia@protonmail.com>
2016-11-12 13:39:57 -0600

    Cambio de prueba

--- app/src/main/java/teamhorario/upslp/apphorariosupslp/WelcomeActivity.java
+++ app/src/main/java/teamhorario/upslp/apphorariosupslp/WelcomeActivity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class WelcomeActivity extends AppCompatActivity {
-
+    int primer = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+
+
     }
 }
