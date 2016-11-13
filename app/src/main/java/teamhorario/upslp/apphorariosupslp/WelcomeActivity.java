77d7f5049df6af395dc433e23cc7581d694dcf7a
ErcokH <Enrique.Munguia@protonmail.com>
2016-11-13 16:02:14 -0600

    commit pantalla principal

--- app/src/main/java/teamhorario/upslp/apphorariosupslp/WelcomeActivity.java
+++ app/src/main/java/teamhorario/upslp/apphorariosupslp/WelcomeActivity.java
@@ -2,15 +2,18 @@ package teamhorario.upslp.apphorariosupslp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class WelcomeActivity extends AppCompatActivity {
-    int primer = 1;
+    Spinner lista;
+    String[] datos = {"ITI","ITEM"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-
-
-
+        lista=(Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,datos);
+        lista.setAdapter(adaptador);
     }
 }
