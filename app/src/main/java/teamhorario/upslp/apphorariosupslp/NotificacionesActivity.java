1a1e10849ff00dfa15026b6f455e7357ff3730b4
ErcokH <Enrique.Munguia@protonmail.com>
2016-11-16 02:08:08 -0600

    Interfaz unida 1

--- app/src/main/java/teamhorario/upslp/apphorariosupslp/NotificacionesActivity.java
+++ app/src/main/java/teamhorario/upslp/apphorariosupslp/NotificacionesActivity.java
@@ -1,31 +1,15 @@
 package teamhorario.upslp.apphorariosupslp;
-
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.R.*;
-import android.view.View;
-import android.widget.Button;
-import android.widget.RelativeLayout;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class NotificacionesActivity extends AppCompatActivity{
 
-    Button siguiente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
-        siguiente= (Button) findViewById(R.id.btnacc);
-        siguiente.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent siguiente = new Intent(WelcomeActivity.this, NotificacionesActivity.class);
-                startActivity(siguiente);
-
-            }
-        });
+        setContentView(R.layout.activity_notificaciones);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
